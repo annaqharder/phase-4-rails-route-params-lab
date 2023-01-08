f3aefc71c06e93ea891e1fb39e3538aa50f64ed5
Anna Harder <annaqharder@gmail.com>
2023-01-07 20:52:20 -0700

    commit tree 8fe6d59cae47279442ead517496c9141c9db2a2f

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,23 @@
 class StudentsController < ApplicationController
 
+  # def index
+  #   students = Student.all
+  #   render json: students
+  # end
+
+  # query params to return a list of all students whose first or last name match the query parameter
   def index
-    students = Student.all
-    render json: students
+    students = if params[:name]
+                  Student.by_name(params[:name])
+              else
+                  Student.all
+              end
+      render json: students
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
